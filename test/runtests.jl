1e227a4b730c3f7b06cc782bdae1b6acf61a6aeb
Robin Deits <robin.deits@gmail.com>
2017-01-23 23:13:10 -0500

    stop using testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,41 @@
-using Compat
 using Base.Test
 using LCMCore
 
-@testset "construct and close" begin
+let
     lcm = LCM()
     close(lcm)
+    close(lcm)
+    close(lcm)
+end
+
+let
+    lcm = LCM()
+
+    did_check = false
+    data = UInt8[1,2,3,4,5]
+    channel = "CHANNEL_1"
+    function check_data(c, d)
+        did_check = true
+        @test c == channel
+        @test d == data
+    end
+    subscribe(lcm, channel, check_data)
+    publish(lcm, channel, data)
+    handle(lcm)
+    @test did_check
+end
+
+let
+    lcm = LCM()
+
+    data = UInt8[1,2,3,4,5]
+    channel = "CHANNEL_1"
+    function check_data(c, d)
+        did_check = true
+        @test c == channel
+        @test d == data
+    end
+    subscribe(lcm, channel, check_data)
+    @async handle(lcm)
+    publish(lcm, channel, data)
 end
