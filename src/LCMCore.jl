8287b11cd48b8e9f12bee8f08b729f03fba13af3
Robin Deits <robin.deits@gmail.com>
2017-01-23 23:04:44 -0500

    store subscriptions in the LCM, and add a 3-argument subscribe
    We need to make sure the subscription callbacks stay in memory, so we
    hold a reference to them inside the LCM object.
    
    Also, adds a 3-argument subscribe() method which does not take a message
    type and just calls the handler with the raw bytes of the message. This
    makes PyLCM really easy to implement

--- src/LCMCore.jl
+++ src/LCMCore.jl
@@ -14,31 +14,36 @@ import Base: unsafe_convert, read, write, close
 using Base.Dates: Period, Millisecond
 export LCM,
        publish,
+       close,
        filedescriptor,
        encode,
        decode,
        subscribe,
-       handle,
-       FingerprintException
+       handle
 
 
 function encode end
 function decode end
-function fingerprint end
 
-immutable FingerprintException <: Exception
-    msg::String
+type SubscriptionOptions{T, F}
+    msgtype::Type{T}
+    handler::F
 end
 
+immutable Subscription{T <: SubscriptionOptions}
+    options::T
+    csubscription::Ptr{Void}
+end
 
 type LCM
     pointer::Ptr{Void}
     filedescriptor::RawFD
+    subscriptions::Vector{Subscription}
 
     LCM() = begin
         pointer = ccall((:lcm_create, liblcm), Ptr{Void}, (Ptr{UInt8},), "")
         filedescriptor = RawFD(ccall((:lcm_get_fileno, liblcm), Cint, (Ptr{Void},), pointer))
-        lc = new(pointer, filedescriptor)
+        lc = new(pointer, filedescriptor, Subscription[])
         finalizer(lc, close)
         lc
     end
@@ -54,16 +59,12 @@ end
 
 function publish{T}(lcm::LCM, channel::AbstractString, msg::T)
     data = encode(msg)
-    status = publish(lcm, convert(String, channel), data)
-    if status == 0
-        return true
-    else
-        return false
-    end
+    publish(lcm, convert(String, channel), data)
 end
 
-function publish(lcm::LCM, channel::String, data::Vector{UInt8})
-    ccall((:lcm_publish, liblcm), Cint, (Ptr{Void}, Ptr{UInt8}, Ptr{UInt8}, Cuint), lcm, channel, data, length(data))
+function publish(lcm::LCM, channel::AbstractString, data::Vector{UInt8})
+    status = ccall((:lcm_publish, liblcm), Cint, (Ptr{Void}, Ptr{UInt8}, Ptr{UInt8}, Cuint), lcm, channel, data, length(data))
+    return status == 0
 end
 
 filedescriptor(lcm::LCM) = lcm.filedescriptor
@@ -75,37 +76,42 @@ immutable RecvBuf
     lcm::Ptr{Void}
 end
 
-type SubscriptionInfo{T, F}
-    msgtype::Type{T}
-    handler::F
-end
-
-function onresponse{T, F}(rbuf::RecvBuf, channelbytes::Ptr{UInt8}, info::SubscriptionInfo{T, F})
+function onresponse{T, F}(rbuf::RecvBuf, channelbytes::Ptr{UInt8}, opts::SubscriptionOptions{T, F})
     channel = unsafe_wrap(String, channelbytes)
     msgdata = unsafe_wrap(Vector{UInt8}, rbuf.data, rbuf.data_size)
-    msg = decode(msgdata, info.msgtype)
-    info.handler(channel, msg)
+    if isa(T, Type{Void})
+        opts.handler(channel, msgdata)
+    else
+        msg = decode(msgdata, opts.msgtype)
+        opts.handler(channel, msg)
+    end
     return nothing::Void
 end
 
-function subscribe{T, F}(lcm::LCM, channel::String, handler::F, msgtype::Type{T})
-    info = SubscriptionInfo{T, F}(msgtype, handler)
-    ccall((:lcm_subscribe, liblcm), Ptr{Void},
-    (Ptr{Void}, Ptr{UInt8}, Ptr{Void}, Ptr{Void}),
-    lcm,
-    channel,
-    cfunction(onresponse, Void, (Ref{RecvBuf}, Ptr{UInt8}, Ref{SubscriptionInfo{T, F}})),
-    Ref(info))
+function subscribe{T <: SubscriptionOptions}(lcm::LCM, channel::String, options::T)
+    csubscription = ccall((:lcm_subscribe, liblcm), Ptr{Void},
+        (Ptr{Void}, Ptr{UInt8}, Ptr{Void}, Ptr{Void}),
+        lcm,
+        channel,
+        cfunction(onresponse, Void, (Ref{RecvBuf}, Ptr{UInt8}, Ref{T})),
+        Ref(options))
+    sub = Subscription(options, csubscription)
+    push!(lcm.subscriptions, sub)
+    sub
 end
 
-chandle(lcm::LCM) = ccall((:lcm_handle, liblcm), Cint, (Ptr{Void},), lcm)
+function subscribe(lcm::LCM, channel::String, handler, msgtype=Void)
+    subscribe(lcm, channel, SubscriptionOptions(msgtype, handler))
+end
+
+lcm_handle(lcm::LCM) = ccall((:lcm_handle, liblcm), Cint, (Ptr{Void},), lcm)
 
 function handle(lcm::LCM)
     fd = filedescriptor(lcm)
     while true
         event = poll_fd(fd, 10; readable=true)
         if event.readable
-            chandle(lcm)
+            lcm_handle(lcm)
             return true
         end
     end
@@ -116,7 +122,7 @@ function handle(lcm::LCM, timeout::Period)
     fd = filedescriptor(lcm)
     event = poll_fd(fd, timeout_ms / 1000; readable=true)
     if event.readable
-        chandle(lcm)
+        lcm_handle(lcm)
         return true
     else
         return false
